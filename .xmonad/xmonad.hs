fd1690f35b9261493f8ca0803fc454fcfe63188b
jthacker <thacker.jon@gmail.com>
2010-10-24 18:36:00 -0400

    adding xmonad and xmobar configs

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,20 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Hooks.ManageDocks
+import System.IO
+
+-- The main function
+main = do
+   xmproc <- spawnPipe "xmobar"
+   xmonad $ defaultConfig
+      {  layoutHook           = avoidStruts $ layoutHook defaultConfig
+      ,  logHook              = dynamicLogWithPP $ xmobarPP
+                                 {  ppOutput = hPutStrLn xmproc
+                                 ,  ppTitle = xmobarColor "green" "" . shorten 50
+                                 }
+      ,  modMask              = mod4Mask
+      ,  terminal             = "gnome-terminal"
+      ,  focusedBorderColor   = "#FFFFFF"
+      ,  normalBorderColor    = "#000000"
+      }
