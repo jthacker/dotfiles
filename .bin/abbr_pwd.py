c8a9c185fb9e70d3b92e55649cd4bcfae696dc61
Jon Thacker <thacker.jon@gmail.com>
2013-07-09 10:15:08 -0500

    changed abbr_pwd.py to handle ctrl-c during startup and not print a stacktrace

--- .bin/abbr_pwd.py
+++ .bin/abbr_pwd.py
@@ -1,34 +1,34 @@
-#! /usr/bin/env python
-import re,os
+#! /usr/bin/env python -S
+def abbreviatedPWD(currentDir = '.', abbreviateLength=20):
+    import re,os
+    homeDir = os.path.expanduser('~')
+    currentDir = os.path.abspath(currentDir)
+    #Check for home directory abbreviation, if found, will replace with '~'
+    p = re.compile(r'(^%s)(.*$)' % homeDir)
+    homeMatch = p.match(currentDir)
+    output = ''
 
-def abbreviatedPWD(currentDir = '.', homeDir = os.path.expanduser('~'), abbreviateLength=20):
-   currentDir = os.path.abspath(currentDir)
-   #Check for home directory abbreviation, if found, will replace with '~'
-   p = re.compile(r'(^%s)(.*$)' % homeDir)
-   homeMatch = p.match(currentDir)
-   output = ''
+    if homeMatch:
+        output = '~'
+        dir = homeMatch.group(2).strip('/')
+        if len(dir) == 0:
+            return output
+        else:
+            dir = dir.split('/')
+    else:
+        dir = currentDir.strip('/').split('/')
 
-   if homeMatch:
-      output = '~'
-      dir = homeMatch.group(2).strip('/')
-
-      if len(dir) == 0:
-         return output
-      else:
-         dir = dir.split('/')
-
-   else:
-      dir = currentDir.strip('/').split('/')
-
-   if len("".join(dir)) > abbreviateLength and len(dir) > 3:
-      output += '/%s/.../%s/%s' % (dir[0], dir[-2], dir[-1])
-   elif len(dir) > 0:
-      output += '/' + '/'.join(dir)
-   
-   return output
+    if len("".join(dir)) > abbreviateLength and len(dir) > 3:
+        output += '/%s/.../%s/%s' % (dir[0], dir[-2], dir[-1])
+    elif len(dir) > 0:
+        output += '/' + '/'.join(dir)
+    
+    return output
 
 if __name__ == '__main__':
-   try:
-      print(abbreviatedPWD())
-   except:
-      print('?')
+    try:
+        print(abbreviatedPWD())
+    except (KeyboardInterrupt, SystemExit):
+        print("")
+    except:
+        print('?')
